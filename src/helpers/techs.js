2648112956bb4232b2448239d2eb619ca867345d
Laura <lauratelqiu@gmail.com>
2023-03-22 15:09:46 -0400

    commit

--- src/helpers/techs.js
+++ src/helpers/techs.js
@@ -1,62 +1,55 @@
-
-
 export const techs = [
   {
-    name: 'React',
-    iconName: 'react'
-  },
-
-
-  {
-    name: 'Javascript',
-    iconName: 'javascript'
-  },
-  {
-    name: 'CSS3',
-    iconName: 'css3'
+    name: "React",
+    iconName: "react",
   },
 
   {
-    name: 'Sass',
-    iconName: 'sass'
+    name: "Javascript",
+    iconName: "javascript",
+  },
+  {
+    name: "CSS3",
+    iconName: "css3",
   },
 
   {
-    name: 'HTML 5',
-    iconName: 'html5'
-  },
-  {
-    name: 'NodeJs',
-    iconName: 'nodejs'
-  },
-
-
-
-  {
-    name: 'NPM',
-    iconName: 'npm'
-  },
-  {
-    name: 'Mongo DB',
-    iconName: 'mongodb'
-  },
-  {
-    name: 'Bootstrap',
-    iconName: 'bootstrap'
+    name: "Sass",
+    iconName: "sass",
   },
 
   {
-    name: 'Django',
-    iconName: 'django'
+    name: "HTML 5",
+    iconName: "html5",
   },
   {
-    name: 'Heroku',
-    iconName: 'heroku'
+    name: "NodeJs",
+    iconName: "nodejs",
+  },
+
+  {
+    name: "NPM",
+    iconName: "npm",
   },
   {
-    name: 'Github',
-    iconName: 'github'
+    name: "Mongo DB",
+    iconName: "mongodb",
+  },
+  {
+    name: "Bootstrap",
+    iconName: "bootstrap",
   },
 
-
-]
+  {
+    name: "Django",
+    iconName: "django",
+  },
+  {
+    name: "Heroku",
+    iconName: "heroku",
+  },
+  {
+    name: "Github",
+    iconName: "github",
+  },
+];
